8abcb9d83287091ae4d9d53cecd11f651fcf0995
Nikhil-dev13 <nikhil13012000@gmail.com>
2021-04-26 16:12:22 +0530

    create production settings

--- server.js
+++ server.js
@@ -14,19 +14,13 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(cors());
 
+app.get("/status", (req, res) => res.send("API up and Running"));
+
 //Defining Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/articles", require("./routes/api/articles"));
 
-//Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
